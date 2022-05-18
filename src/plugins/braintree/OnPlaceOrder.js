1d03a902e64a68076c68d983cd75e235cef96cea
Sy Huong <syhuong@apples-MacBook-Pro.local>
2022-05-18 14:57:16 +0700

    Update

--- src/plugins/braintree/OnPlaceOrder.js
+++ src/plugins/braintree/OnPlaceOrder.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BraintreeDropIn from 'react-native-braintree-payments-drop-in';
+// import BraintreeDropIn from 'react-native-braintree-payments-drop-in';
 import Identify from '@helper/Identify';
 import material from '@theme/variables/material';
 import { Toast } from 'native-base';
@@ -9,21 +9,21 @@ export function onPlaceOrder(parent) {
     if(parent.selectedPayment.payment_method.toUpperCase() != 'SIMIBRAINTREE') {
         return false;
     }
-    BraintreeDropIn.show({
-        clientToken: parent.selectedPayment.token,
-    }).then(result => {
-        console.log(result);
-        parent.onPlaceOrder({
-            nonce: result.nonce,
-            type: result.type,
-            description: result.description
-        });
-    }).catch((error) => {
-        console.log(error);
-        Toast.show({
-            text: Identify.__('Payment failed. Please try again'),
-            type: "danger", textStyle: { fontFamily: material.fontFamily }, duration: 3000
-        })
-    });
+    // BraintreeDropIn.show({
+    //     clientToken: parent.selectedPayment.token,
+    // }).then(result => {
+    //     console.log(result);
+    //     parent.onPlaceOrder({
+    //         nonce: result.nonce,
+    //         type: result.type,
+    //         description: result.description
+    //     });
+    // }).catch((error) => {
+    //     console.log(error);
+    //     Toast.show({
+    //         text: Identify.__('Payment failed. Please try again'),
+    //         type: "danger", textStyle: { fontFamily: material.fontFamily }, duration: 3000
+    //     })
+    // });
     return true;
 }
